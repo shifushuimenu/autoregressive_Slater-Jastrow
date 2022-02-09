5d75b4bdaede3de1678cb5e6f24bd79474d16737
aloma <aloma@kabir>
2022-02-09 18:05:35 +0100

    1d nn. hopping matrix, correction factors work (inefficient)

--- reference_state.py
+++ reference_state.py
@@ -1,3 +1,4 @@
+from curses import KEY_SCOPY
 import numpy as np
 from test_suite import prepare_test_system_zeroT
 
@@ -19,12 +20,21 @@ one_hop_conf = xs[1:]
 # `k_copy` indicates the component up to which (inclusive)
 # the conditional probabilities are identical to those 
 # of the reference state (such that they can be copied).
-k_copy = (-1, # don't copy any conditional probabilities  
-           0,
-           0,
+k_copy = ( 0, # The first component is conditionally independent, it can always be copied from the reference state. 
+           1, # copy conditional probs up to (inclusive) the second component 
            1,
-           1,
-           2)
+           2, # copy conditional probs up to (inclusive) the third component
+           2, 
+           3)
+
+def monotonically_increasing(y):
+    r = True 
+    for i in range(len(y)-1):
+        r = r and (y[i] <= y[i+1])
+        if not r: return r 
+    return r 
+
+assert monotonically_increasing(k_copy)
 
 # The "one-hop" config is generated from the reference state by 
 # removing a particle at position `r` and putting it 
@@ -36,7 +46,10 @@ rs_pos = ( (0,1),
            (4,5),
            (7,6) )
 
-one_hop_info = dict(k_copy=k_copy, rs_pos=rs_pos, xs=one_hop_conf)
+# special case of 1d n.n. hopping matrix 
+assert np.all([abs(r-s) == 1 for r,s in rs_pos])
+
+one_hop_info = list(zip(k_copy, rs_pos))
 
 def corr_factor_removeadd_rs(Ainv, r, s):
     """ 
@@ -62,14 +75,27 @@ def adapt_Gdenom(Gnum, r, s):
     G[s,s] = G[s,s] - 1
     return G
 
+def reduce_Gdenom(Gdenom, r, s):
+    assert r > s 
+    assert Gdenom.shape == (r+1, r+1)
+    G = Gdenom[np.ix_(list(range(0, s+1)), list(range(0, s+1)))]
+    G[s,s] = G[s,s] - 1
+    return G
+
+def reduce_Gnum(Gdenom, r, s): # yes, Gdenom is the argument !
+    assert r > s 
+    assert Gdenom.shape == (r+1, r+1)
+    G = Gnum[np.ix_(list(range(0, r+1)), list(range(0, r+1)))]
+    G[s,s] = G[s,s] - 1
+    # G[r,r] = G[r,r] # Now, there is a particle both at position r and s. 
+    return G
+
 
 # Calculate the conditional probabilities of the reference state
 Ns = 9; Np = 5
 _, U = prepare_test_system_zeroT(Nsites=Ns, potential='none', Nparticles=Np)
 P = U[:, 0:Np]
 G = np.eye(Ns) - np.matmul(P, P.transpose(-1,-2))
-
-print("G_global=", G[0:4, 0:4])
 
 cond_prob_ref = np.zeros((Np, Ns))
 cond_prob_onehop = np.zeros((len(xs[1:]), Np, Ns))
@@ -93,26 +119,59 @@ for k in range(Np):
         # Now calculate the conditional probabilities for all states related 
         # to the reference state by one hop, using a low-rank update of `Gnum`
         # and `Gdenom`.
-        r=0; s=1
-        if k > 0 and i > s: 
-            if not np.isclose(np.linalg.det(Gnum), 0.0):
+        for state_nr, (k_copy, (r,s)) in enumerate(one_hop_info):
+            print("state_nr=", state_nr)
+            if k_copy >= k:
+                break # copy conditional probabilities rather than calculating them 
+            else: # k_copy < k
+                if s > r:
+                    if i > s:
+                        if not np.isclose(np.linalg.det(Gnum), 0.0): # don't invert a singular matrix 
+                            print(k, ii, i, cond_prob_ref[k,i])
+                            Gnum_inv = np.linalg.inv(Gnum)
+                            if k==(k_copy+1):                     
+                                corr_factor = corr_factor_removeadd_rs(Gnum_inv, r=r, s=s) \
+                                            * np.linalg.det(Gdenom)
+                                Gdenom_ = adapt_Gdenom(Gnum, r=r, s=s)
+                                corr_factor /= np.linalg.det(Gdenom_)
+                            else:
+                                Gdenom_inv = np.linalg.inv(Gdenom)
+                                corr_factor = corr_factor_removeadd_rs(Gnum_inv, r=r, s=s) \
+                                                / corr_factor_removeadd_rs(Gdenom_inv, r=r, s=s) 
+                            cond_prob_onehop[state_nr, k, i] = corr_factor * cond_prob_ref[k, i]
+                        else: 
+                            cond_prob_onehop[state_nr, k, i] = 0.0
+                elif r > s: 
+                    # The support is larger than in the reference state. One needs to calculate (r-s)
+                    # more conditional probabilities than in the reference state. 
+                    # In other words, here,  i not in (xmin, xmax). 
+                    # ... TODO ...
+                    # The case i == r is special. 
+                    if not np.isclose(np.linalg.det(Gnum), 0.0): # don't invert a singular matrix      
+                        Gnum_inv = np.linalg.inv(Gnum)                                                   
+                        if k==(k_copy+1):
+                            Gdenom_ = reduce_Gdenom(Gdenom, r=r, s=s)
+                            if i==(r+1):
+                                # Actually we wish to calculate i==r, but since such an i is not in (xmin, xmax),
+                                # it will never appear in the iteration. Therefore this case is treated explicitly 
+                                # here. Since this case does not appear for the reference state, a "correction factor"
+                                # is not calculated, instead the cond. prob. is calculated directly:
+                                Gnum_ = reduce_Gnum(Gdenom, r=r, s=s)
+                                cond_prob_onehop[state_nr, k, i-1] = (-1) * np.linalg.det(Gnum_) / np.linalg.det(Gdenom_)
+                                pass
+                            if i > r:  
+                                corr_factor = corr_factor_removeadd_rs(Gnum_inv, r=r, s=s) \
+                                        * (np.linalg.det(Gdenom) / np.linalg.det(Gdenom_))
+                        else:
+                            Gdenom_inv = np.linalg.inv(Gdenom)
+                            corr_factor = corr_factor_removeadd_rs(Gnum_inv, r=r, s=s) \
+                                            / corr_factor_removeadd_rs(Gdenom_inv, r=r, s=s) 
+                        cond_prob_onehop[state_nr, k, i] = corr_factor * cond_prob_ref[k, i]
+                    else:
+                        cond_prob_onehop[state_nr, k, i] = 0.0
 
-                print(k, ii, i, cond_prob_ref[k,i])
-                print("Gnum.shape=", Gnum.shape)
-                print("Gdenom.shape=", Gdenom.shape)
-                Gnum_inv = np.linalg.inv(Gnum)
-                if k==1:                     
-                    corr_factor = corr_factor_removeadd_rs(Gnum_inv, r=r, s=s) \
-                                  * np.linalg.det(Gdenom)
-                    Gdenom_ = adapt_Gdenom(Gnum, r=r, s=s)
-                    corr_factor /= np.linalg.det(Gdenom_)
-                else:
-                    Gdenom_inv = np.linalg.inv(Gdenom)
-                    corr_factor = corr_factor_removeadd_rs(Gnum_inv, r=r, s=s) \
-                                    / corr_factor_removeadd_rs(Gdenom_inv, r=r, s=s) 
-                cond_prob_onehop[0, k, i] = corr_factor * cond_prob_ref[k, i]
-            else: 
-                cond_prob_onehop[0, k, i] = 0.0
 
 assert np.isclose(np.sum(cond_prob_ref, axis=1), np.ones((Np,1))).all()
-print("cond_prob_onehop[0, :, :]=", cond_prob_onehop[0, :, :])
+for state_nr in range(6):
+    print("cond_prob_onehop[%d, :, :]="%(state_nr), cond_prob_onehop[state_nr, :, :])
+    print(np.sum(cond_prob_onehop[state_nr, :, :], axis=1))
