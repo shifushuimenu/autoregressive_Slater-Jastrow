a3fb3713818bb2c93e58ae5dd80c0079eab2ac22
Stephan Humeniuk <stephan.humeniuk@gmail.com>
2022-06-10 14:19:50 -0400

    add stochastic reconfiguration sr.py

---
+++ sr.py
@@ -0,0 +1,19 @@
+"""Stochastic reconfiguration
+
+References:
+===========
+[1] S. Sorella, Green Function Monte Carlo with Stochastic Recon-
+    figuration, Phys. Rev. Lett. 80, 4558 (1998).
+[2] S. Sorella, Wave function optimization in the variational Monte
+    Carlo method, Phys. Rev. B 71, 241103(R) (2005).
+[3] S.-I. Amari, Natural gradient works efficiently in learning,
+    Neural Comput. 10, 251 (1998).
+"""
+
+class SR():
+    def  __init__(self):
+
+    def calc_Fisher_infomatrix(self, log_probs):
+        """
+        """
+        pass
