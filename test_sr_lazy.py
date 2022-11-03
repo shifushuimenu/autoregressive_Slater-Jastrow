7e970cb5b0f33c2109c1dc56bc1980eae1de480a
Stephan Humeniuk <stephan.humeniuk@gmail.com>
2022-11-03 14:44:22 -0400

    add test for sr_lazy.py

--- test_sr_lazy.py
+++ test_sr_lazy.py
@@ -8,7 +8,7 @@ class TestSR(unittest.TestCase):
     def test_lazy_matrix_representation(self, tol=1e-8):
         # linear operator representing the overlap matrix 
         npara = 100; ns = 1000
-        SR = SR_Preconditioner_base(num_params=npara, num_samples=ns, diag_shift=0.0, dtype=np.float64)
+        SR = SR_Preconditioner_base(num_params=npara, num_samples=ns, eps1=0.0, eps2=0.0, dtype=np.float64)
         O_ks = np.random.randn(npara, ns)
         for ii in range(ns):
             SR.accumulate(O_ks[:, ii])
@@ -19,7 +19,7 @@ class TestSR(unittest.TestCase):
         x1 = SR.apply_Sinv(b, tol=tol)
 
         Smat = SR._to_dense()
-        assert np.all(np.isclose(SR.scale, np.diag(Smat), atol=tol))
+        assert np.all(np.isclose(SR.diagme, np.diag(Smat), atol=tol)) # SR.scale -> SR.diagme
 
         x2 = np.linalg.inv(Smat) @ b
         assert np.all(np.isclose(x1, x2, atol=tol))        
