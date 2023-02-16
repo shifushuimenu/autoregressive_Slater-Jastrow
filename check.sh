df91aaed5bf58da01d126b7039737670dd87b222
Stephan Humeniuk <stephan.humeniuk@gmail.com>
2023-02-16 11:40:09 -0500

    add check.sh

---
+++ check.sh
@@ -0,0 +1,4 @@
+#python3 ./run_autoregVMC.py 4 4 8 3.0 4 4 4 --seed 42
+#ene= 3.22025248948197
+## 4 samples in 0.324203 seconds
+python3 ./run_autoregVMC.py 4 4 4 3.0 4 40 4 --seed 42 --optimize_orbitals True
